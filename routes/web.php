154d951a69ccc4e308976fac18e8a1c8033b580b
RonenTGreat <ronenh53@gmail.com>
2024-10-22 21:45:05 +0000

    added auth routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+Route::get('/signup', function () {
+    return view('auth/signup');
+});
+
+Route::get('/user-login', function () {
+    return view('auth/user-login');
+});
+
